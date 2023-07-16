3c9f07ed4a29ba9bfb743552401d2e2e2f1bbdba
Petrol <35155443+Chizz3x@users.noreply.github.com>
2023-07-16 04:38:44 +0300

    sitemap fix???

--- src/tools/gen-sitemap.ts
+++ src/tools/gen-sitemap.ts
@@ -16,7 +16,7 @@ const OUTPUT_FILE = path.resolve("public", "sitemap.xml");
 			<?xml version="1.0" encoding="UTF-8"?>
 			<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 				${Object.values(ROUTES).map(route => `<url>
-					<loc>${path.posix.join(env.PUBLIC_URL, route)}</loc>
+					<loc>${path.posix.join(env.PUBLIC_URL, "?", route)}</loc>
 				</url>`).join("\n")}
 			</urlset>
 		`,
